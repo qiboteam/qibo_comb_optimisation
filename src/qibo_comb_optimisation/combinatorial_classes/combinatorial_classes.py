481e4d04f65139d67d10f30a749858dbe81a0ff1
shangtai <christo173@hotmail.com>
2024-09-14 03:50:23 +0800

    repatch missing files

---
+++ src/qibo_comb_optimisation/combinatorial_classes/combinatorial_classes.py
@@ -0,0 +1,305 @@
+"""
+This module comprises of various applications that are commonly formulated in QUBO formulations.
+"""
+
+import numpy as np
+from qibo import gates
+from qibo.backends import _check_backend
+from qibo.hamiltonians import SymbolicHamiltonian
+from qibo.models.circuit import Circuit
+from qibo.symbols import X, Y, Z
+from src.qibo_comb_optimisation.optimization_class.quadratic_problem import linear_problem, quadratic_problem
+
+
+def calculate_two_to_one(num_cities):
+    """
+    :param num_cities: number of cities for TSP
+    :return: a 2 by 2 numpy matrix encoding the mapping from two coordinate to one cooedinate
+    """
+    return np.arange(num_cities**2).reshape(num_cities, num_cities)
+
+
+def tsp_phaser(distance_matrix, backend=None):
+    """
+    Args:
+        distance_matrix: this is a numpy matrix
+        backend: backend to be specified
+
+    Returns: The phaser hamiltonian for TSP
+
+    """
+    num_cities = distance_matrix.shape[0]
+    two_to_one = calculate_two_to_one(num_cities)
+    form = 0
+    for i in range(num_cities):
+        for u in range(num_cities):
+            for v in range(num_cities):
+                if u != v:
+                    form += (
+                        distance_matrix[u, v]
+                        * Z(int(two_to_one[u, i]))
+                        * Z(int(two_to_one[v, (i + 1) % num_cities]))
+                    )
+    ham = SymbolicHamiltonian(form, backend=backend)
+    return ham
+
+
+def tsp_mixer(num_cities, backend=None):
+    """
+
+    Args:
+        num_cities: The number of cities
+        backend: Backend to be specified
+
+    Returns: TSP mixer
+
+    """
+    two_to_one = calculate_two_to_one(num_cities)
+
+    def splus(u, i):
+        """
+        Args:
+            u: index for cities
+            i: index for positions
+        Returns: Splus hamiltonian
+        """
+        return X(int(two_to_one[u, i])) + 1j * Y(int(two_to_one[u, i]))
+
+    def sminus(u, i):
+        """
+        Args:
+            u: index for cities
+            i: index for positions
+        Returns: Splus hamiltonian
+        """
+        return X(int(two_to_one[u, i])) - 1j * Y(int(two_to_one[u, i]))
+
+    form = 0
+    for i in range(num_cities):
+        for u in range(num_cities):
+            for v in range(num_cities):
+                if u != v:
+                    form += splus(u, i) * splus(v, (i + 1) % num_cities) * sminus(
+                        u, (i + 1) % num_cities
+                    ) * sminus(v, i) + sminus(u, i) * sminus(
+                        v, (i + 1) % num_cities
+                    ) * splus(
+                        u, (i + 1) % num_cities
+                    ) * splus(
+                        v, i
+                    )
+    ham = SymbolicHamiltonian(form, backend=backend)
+    return ham
+
+
+class TSP:
+    """
+    The travelling salesman problem (also called the travelling salesperson problem or TSP)
+    asks the following question: "Given a list of cities and the distances between each pair
+    of cities,what is the shortest possible route for a salesman to visit each city exactly
+    once and return to the origin city?"
+    It is an NP-hard problem in combinatorial optimization.
+    It is also important in theoretical computer science and
+    operations research.
+
+    This is a TSP class that enables us to implement TSP according to
+    `arxiv:1709.03489 <https://arxiv.org/abs/1709.03489>`_ by Hadfield (2017).
+
+    Args:
+            distance_matrix: a numpy matrix encoding the distance matrix.
+            backend: Backend to use for calculations. If not given the global backend will be used.
+
+    Example:
+        .. testcode::
+
+            from qibo.models.tsp import TSP
+            import numpy as np
+            from collections import defaultdict
+            from qibo import gates
+            from qibo.models import QAOA
+            from qibo.result import CircuitResult
+
+
+            def convert_to_standard_Cauchy(config):
+                m = int(np.sqrt(len(config)))
+                cauchy = [-1] * m  # Cauchy's notation for permutation, e.g. (1,2,0) or (2,0,1)
+                for i in range(m):
+                    for j in range(m):
+                        if config[m * i + j] == '1':
+                            cauchy[j] = i  # citi i is in slot j
+                for i in range(m):
+                    if cauchy[i] == 0:
+                        cauchy = cauchy[i:] + cauchy[:i]
+                        return tuple(cauchy)
+                        # now, the cauchy notation for permutation begins with 0
+
+
+            def evaluate_dist(cauchy):
+                '''
+                Given a permutation of 0 to n-1, we compute the distance of the tour
+
+                '''
+                m = len(cauchy)
+                return sum(distance_matrix[cauchy[i]][cauchy[(i+1)%m]] for i in range(m))
+
+
+            def qaoa_function_of_layer(layer, distance_matrix):
+                '''
+                This is a function to study the impact of the number of layers on QAOA,
+                it takes in the number of layers and compute the distance of the mode
+                of the histogram obtained from QAOA
+
+                '''
+                small_tsp = TSP(distance_matrix)
+                obj_hamil, mixer = small_tsp.hamiltonians()
+                qaoa = QAOA(obj_hamil, mixer=mixer)
+                best_energy, final_parameters, extra = qaoa.minimize(initial_p=[0.1] * layer,
+                                                     initial_state=initial_state, method='BFGS')
+                qaoa.set_parameters(final_parameters)
+                quantum_state = qaoa.execute(initial_state)
+                circuit = Circuit(9)
+                circuit.add(gates.M(*range(9)))
+                result = CircuitResult(quantum_state, circuit.measurements,
+                        small_tsp.backend, nshots=1000)
+                freq_counter = result.frequencies()
+                # let's combine freq_counter here, first convert each key and sum up the frequency
+                cauchy_dict = defaultdict(int)
+                for freq_key in freq_counter:
+                    standard_cauchy_key = convert_to_standard_Cauchy(freq_key)
+                    cauchy_dict[standard_cauchy_key] += freq_counter[freq_key]
+                max_key = max(cauchy_dict, key=cauchy_dict.get)
+                return evaluate_dist(max_key)
+
+            np.random.seed(42)
+            num_cities = 3
+            distance_matrix = np.array([[0, 0.9, 0.8], [0.4, 0, 0.1],[0, 0.7, 0]])
+            distance_matrix = distance_matrix.round(1)
+            small_tsp = TSP(distance_matrix)
+            initial_parameters = np.random.uniform(0, 1, 2)
+            initial_state = small_tsp.prepare_initial_state([i for i in range(num_cities)])
+            qaoa_function_of_layer(2, distance_matrix)
+
+    """
+
+    def __init__(self, distance_matrix, backend=None):
+        self.backend = _check_backend(backend)
+
+        self.distance_matrix = distance_matrix
+        self.num_cities = distance_matrix.shape[0]
+        self.two_to_one = calculate_two_to_one(self.num_cities)
+
+    def hamiltonians(self):
+        """
+        Returns:
+            The pair of Hamiltonian describes the phaser hamiltonian
+            and the mixer hamiltonian.
+
+        """
+        return (
+            tsp_phaser(self.distance_matrix, backend=self.backend),
+            tsp_mixer(self.num_cities, backend=self.backend),
+        )
+
+    def prepare_initial_state(self, ordering):
+        """
+        To run QAOA by Hadsfield, we need to start from a valid
+        permutation function to ensure feasibility.
+
+        Args:
+            ordering (array): A list describing permutation from 0 to n-1
+
+        Returns:
+            An initial state that is used to start TSP QAOA.
+
+        """
+        n = len(ordering)
+        c = Circuit(n**2)
+        for i in range(n):
+            c.add(gates.X(int(self.two_to_one[ordering[i], i])))
+        result = self.backend.execute_circuit(c)
+        return result.state()
+
+    def penalty_method(self, penalty):
+        """ "
+        return: a TSP QUBO object that is constructed based on penalty method
+        """
+        q_dict = {}
+        for u in range(self.num_cities):
+            for v in range(self.num_cities):
+                if v != u:
+                    for j in range(self.num_cities):
+                        # this is the objective function
+                        q_dict[
+                            self.two_to_one[u, j],
+                            self.two_to_one[v, (j + 1) % (self.num_cities + 1)],
+                        ] = self.distance_matrix[u, v]
+        qp = quadratic_problem(q_dict)
+        # row constraints
+
+        for v in range(self.num_cities):
+            row_constrait = [0 for _ in range(self.num_cities**2)]
+            for j in range(self.num_cities):
+                row_constrait[self.two_to_one(v, j)] = 1
+            lp = linear_problem(row_constrait, -1)
+            tmp_qp = lp.square()
+            tmp_qp.multiply_scalar(penalty)
+            qp = qp + tmp_qp
+
+        # column constraints
+        for j in range(self.num_cities):
+            col_constrait = [0 for _ in range(self.num_cities**2)]
+            for v in range(self.num_cities):
+                col_constrait[self.two_to_one(v, j)] = 1
+            lp = linear_problem(col_constrait, -1)
+            tmp_qp = lp.square()
+            tmp_qp.multiply_scalar(penalty)
+            qp = qp + tmp_qp
+        return qp
+
+
+class Mis:
+    """
+    mis (maximal independent set) is a popular combinatorial optimization problem,
+    this module helps to prepare the hamiltonian so that the user can use Qibo to
+    explore combnatorial problem easily
+    """
+
+    def __init__(self, g):
+        """
+
+        :param self:
+        :param g:  A networkx object
+        :return: a QUBO representation
+        """
+        self.g = g
+        self.n = g.number_of_nodes()
+
+    def penalty_method(self, penalty):
+        """
+        Args:
+            penalty: Penalty parameter for MIS
+
+        Returns: A hamiltonian correspondng to the penalty method for mis
+
+        """
+        q_dict = {}
+        for i in range(self.n):
+            q_dict[(i, i)] = -1
+        for u, v in self.g.edges:
+            q_dict[(u, v)] = penalty
+        print(q_dict)
+        return quadratic_problem(0, q_dict)
+
+    def __str__(self):
+        return self.__class__.__name__
+
+import networkx as nx
+g = nx.Graph()
+g.add_edges_from([(0, 1), (1, 2), (2, 0)])
+mis = Mis(g)
+print(mis.n)
+
+penalty = 10
+qp = mis.penalty_method(penalty)
+print(type(qp))
+
---
+++ src/qibo_comb_optimisation/optimization_class/quadratic_problem.py
@@ -0,0 +1,302 @@
+import itertools
+
+import numpy as np
+from qibo import hamiltonians
+from qibo.symbols import Z
+
+
+class quadratic_problem:
+    def __init__(self, offset, *args):
+        """
+        :param args: this can either be a dictionary or h and J for typical ising formulation
+        :param offset: an integer
+
+                .. math::
+
+             s'  J  s + h'  s = offset + x'  Q x
+
+        Args:
+            h (dict[variable, bias]):
+                Linear biases as a dict of the form {v: bias, ...}, where keys are variables of
+                the model and values are biases.
+            J (dict[(variable, variable), bias]):
+               Quadratic biases as a dict of the form {(u, v): bias, ...}, where keys
+               are 2-tuples of variables of the model and values are optimization_class biases
+               associated with the pair of variables (the interaction).
+            offset (numeric, optional, default=0):
+                Constant offset to be applied to the energy. Default 0.
+        """
+
+        self.offset = offset
+        print(len(args))
+        print(isinstance(args[0], dict))
+        if len(args) == 1 and isinstance(args[0], dict):
+            self.Qdict = args[0]
+            self.n = 0
+            for key in self.Qdict:
+                self.n = max([self.n, key[0], key[1]])
+            self.n += 1
+        else:
+            h = args[0]
+            J = args[1]
+            self.Qdict = {(v, v): 2.0 * bias for v, bias in h.items()}
+            self.n = 0
+
+            # next the optimization_class biases
+            for (u, v), bias in self.Qdict.items():
+                if bias == 0.0:
+                    continue
+                self.Qdict[(u, v)] = 4.0 * bias
+                self.Qdict[(u, u)] = self.Qdict.setdefault((u, u), 0) - 2.0 * bias
+                self.Qdict[(v, v)] = self.Qdict.setdefault((v, v), 0) - 2.0 * bias
+                self.n = max([self.n, u, v])
+            self.n += 1
+            # finally adjust the offset based on QUBO definitions rather than Ising formulation
+            self.offset += sum(args.values()) - sum(h.values())
+
+    def multiply_scalar(self, scalar_multiplier):
+        """
+        :param scalar: this is the scalar that we want to multiply the optimization_class function to
+        :return: None, just updating the optimization_class function
+        """
+        for key in self.Qdict:
+            self.Qdict[key] *= scalar_multiplier
+        self.offset *= scalar_multiplier
+
+    def __add__(self, other_Quadratic):
+        """
+        :param other_Quadratic: another optimization_class class object
+        :return: updating the optimization_class function to obtain the sum
+        """
+        for key in other_Quadratic.Qdict:
+            if key in self.Qdict:
+                self.Qdict[key] += other_Quadratic.Qdict[key]
+            else:
+                self.Qdict[key] = other_Quadratic.Qdict[key]
+        self.n = max(self.n, other_Quadratic.n)
+        self.offset += other_Quadratic.offset
+
+    def qubo_to_ising(self, constant=0.0):
+        """Convert a QUBO problem to an Ising problem.
+
+        Map a optimization_class unconstrained binary optimization (QUBO) problem  defined over binary variables
+        (0 or 1 values), where the linear term is contained along x' Qx
+        the diagonal of Q, to an Ising model defined on spins (variables with {-1, +1} values).
+        Return h and J that define the Ising model as well as the offset in energy
+        between the two problem formulations:
+
+        .. math::
+
+             x'  Q  x  = constant + s'  J  s + h'  s
+
+        Args:
+            Q (dict[(variable, variable), coefficient]):
+                QUBO coefficients in a dict of form {(u, v): coefficient, ...}, where keys
+                are 2-tuples of variables of the model and values are biases
+                associated with the pair of variables. Tuples (u, v) represent interactions
+                and (v, v) linear biases.
+            constant:
+                Constant offset to be applied to the energy. Default 0.
+
+        Returns:
+            (dict, dict, float): A 3-tuple containing:
+
+                dict: Linear coefficients of the Ising problem.
+
+                dict: Quadratic coefficients of the Ising problem.
+
+                float: New energy offset.
+
+        Examples:
+            This example converts a QUBO problem of two variables that have positive
+            biases of value 1 and are positively coupled with an interaction of value 1
+            to an Ising problem, and shows the new energy offset.
+
+
+        """
+        h = {}
+        J = {}
+        linear_offset = 0.0
+        quadratic_offset = 0.0
+
+        for (u, v), bias in self.Qdict.items():
+            if u == v:
+                if u in h:
+                    h[u] += bias / 2
+                else:
+                    h[u] = bias / 2
+                linear_offset += bias
+
+            else:
+                if bias != 0.0:
+                    J[(u, v)] = bias / 4
+
+                if u in h:
+                    h[u] += bias / 4
+                else:
+                    h[u] = bias / 4
+
+                if v in h:
+                    h[v] += bias / 4
+                else:
+                    h[v] = bias / 4
+
+                quadratic_offset += bias
+
+        constant += 0.5 * linear_offset + 0.25 * quadratic_offset
+
+        return h, J, constant
+
+    def construct_symbolic_Hamiltonian_from_QUBO(self):
+        """
+
+        :param Q: Q, a matrix describing the QUBO
+        :return: a symbolic hamiltonian that corresponds to the QUBO
+        """
+        h, J, constant = self.Qdict.qubo_to_ising(self.Qdict)
+        symbolic_ham = sum(h[i] * Z[i] for i in h)
+        symbolic_ham += sum(J[u, v] * Z[u] * Z[v] for (u, v) in J)
+        symbolic_ham += constant
+        ham = hamiltonians.SymbolicHamiltonian(symbolic_ham)
+        return ham
+
+    def evaluate_f(self, x):
+        f_value = self.offset
+        for i in range(self.n):
+            if x[i] != 0:
+                # manage diagonal term first
+                if (i, i) in self.Qdict:
+                    f_value += self.Qdict[(i, i)]
+                    for j in range(i + 1, self.n):
+                        if x[j] != 0:
+                            f_value += self.Qdict.get((i, j), 0) + self.Qdict.get(
+                                (j, i), 0
+                            )
+        return f_value
+
+    def evaluate_grad_f(self, x):
+        """
+        :param x:
+        :return: grad, the corresponding gradient
+        """
+        grad = np.asarray([self.Qdict.get((i, i), 0) for i in range(self.n)])
+        for i in range(self.n):
+            for j in range(self.n):
+                if j != i and x[j] == 1:
+                    grad[i] += self.Qdict.get((i, j), 0) + self.Qdict.get((j, i), 0)
+        return grad
+
+    def tabu_search(self, max_iterations=100, tabu_size=10):
+        """
+        Perform tabu search to find a minimizer for a QUBO problem.
+
+        Args:
+        - max_iterations (int): Maximum number of iterations to perform.
+        - tabu_size (int): Size of the tabu list.
+
+        Returns:
+        - numpy.array: Binary vector representing the minimizer found.
+        - float: Objective value of the minimizer found.
+        """
+        x = np.random.randint(2, size=self.n)  # Initial solution
+        best_solution = x.copy()
+        best_obj_value = self.evaluate_f(x)
+        tabu_list = []
+
+        for _ in range(max_iterations):
+            neighbors = []
+            for i in range(self.n):
+                neighbor = x.copy()
+                neighbor[i] = 1 - neighbor[i]  # Flip a bit
+                neighbors.append((neighbor, self.evaluate_f(neighbor)))
+
+            # Choose the best neighbor that is not tabu
+            best_neighbor = min(neighbors, key=lambda x: x[1])
+            best_neighbor_solution, best_neighbor_obj = best_neighbor
+
+            # Update the current solution if it's better than the previous best and not tabu
+            if (
+                best_neighbor_obj < best_obj_value
+                and best_neighbor_solution.tolist() not in tabu_list
+            ):
+                x = best_neighbor_solution
+                best_solution = x.copy()
+                best_obj_value = best_neighbor_obj
+
+            # Add the best neighbor to the tabu list
+            tabu_list.append(best_neighbor_solution.tolist())
+            if len(tabu_list) > tabu_size:
+                tabu_list.pop(0)
+
+        return best_solution, best_obj_value
+
+    def brute_force(self):
+        # this is to solve the QUBO in a brute force fashion for benchmarking purpose.
+        # note that this approach is very slow.
+        possible_values = {}
+        # A list of all the possible permutations for x vector
+        vec_permutations = itertools.product([0, 1], repeat=self.n)
+
+        for permutation in vec_permutations:
+            x = np.array(
+                [[var] for var in permutation]
+            )  # Converts the permutation into a column vector
+            value = self.evaluate_f(x)
+            possible_values[value] = (
+                x  # Adds the value and its vector to the dictionary
+            )
+
+        min_value = min(
+            possible_values.keys()
+        )  # Lowest value of the objective function
+        opt_vector = tuple(
+            possible_values[min_value].T[0]
+        )  # Optimum vector x that produces the lowest value
+
+        return opt_vector, min_value
+
+
+class linear_problem:
+    def __init__(self, A, b):
+        """
+        :param A: A is a numpy matrix, possibly a row vector
+        """
+        self.A = A
+        self.b = b
+        self.n = len(b)
+
+    def multiply_scalar(self, scalar_multiplier):
+        """
+        :param scalar: this is the scalar that we want to multiply the optimization_class function to
+        :return: None, just updating the optimization_class function
+        """
+        self.A *= scalar_multiplier
+        self.b *= scalar_multiplier
+
+    def __add__(self, other_linear):
+        """
+        :param other_Quadratic: another optimization_class class object
+        :return: updating the optimization_class function to obtain the sum
+        """
+        self.A += other_linear.A
+        self.b += other_linear.b
+        return self
+
+    def evaluate_f(self, x):
+        return self.A @ x + self.b
+
+    def square(self):
+        """
+        if we square a linear term, we should obtain a QUBO
+        ||Ax+b||^2 = (Ax+b)^T(Ax+b) =x^TA^TAx =+ 2b^TAx + b^Tb
+        =X^T(A^TA - diag(A^TA))x + (diag(A^TA)+2b^TA)x + b^Tb
+        :return: a QUBO object
+        """
+        quadraticpart = self.A.T @ self.A + np.diag(2 * (self.b @ self.A))
+        offset = np.dot(self.b, self.b)
+        num_rows, num_cols = quadraticpart.shape
+        Qdict = dict()
+        for i in range(num_rows):
+            for j in range(num_cols):
+                Qdict[(i, j)] = quadraticpart[i, j]
+        return quadratic_problem(offset, Qdict)
