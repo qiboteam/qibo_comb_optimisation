d2ad8873157046b4f59a5cca08fe89c0db28b04c
shangtai <christo173@hotmail.com>
2024-09-16 16:57:12 +0800

    remove src form tests files

--- tests/test_combinatorial_classes.py
+++ tests/test_combinatorial_classes.py
@@ -1,8 +1,8 @@
 import numpy as np
 import networkx as nx
 from qibo.hamiltonians import SymbolicHamiltonian
-from src.qibo_comb_optimisation.combinatorial_classes.combinatorial_classes import calculate_two_to_one, tsp_phaser, tsp_mixer, TSP, Mis
-from src.qibo_comb_optimisation.optimization_class.optimization_class import quadratic_problem
+from qibo_comb_optimisation.combinatorial_classes.combinatorial_classes import calculate_two_to_one, tsp_phaser, tsp_mixer, TSP, Mis
+from qibo_comb_optimisation.optimization_class.optimization_class import quadratic_problem
 
 
 def test_calculate_two_to_one():
