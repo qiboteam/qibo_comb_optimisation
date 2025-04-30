4dc1b8ecee5f88aec66fbee9f38bfa8750f74257
J F Kong <jfkong34@gmail.com>
2025-05-01 03:31:46 +0800

    modified edge cases test

--- tests/test_optimisation_class.py
+++ tests/test_optimisation_class.py
@@ -413,9 +413,24 @@ def test_train_QAOA_svp_mixer(gammas, betas, alphas, reg_loss, cvar_delta):
     name_to_index = {"w[1]": 0, "w[2]": 1, "x_1_0": 2, "x_2_0": 3, "y[1]": 4, "y[2]": 5}
 
 
+
     # SVP_mixers is now a list of functions that take beta and return a circuit
     SVP_mixers = [lambda beta, idx=idx: _create_svp_mixer(name_to_index, beta) for idx in range(len(betas))]
 
+    # Edge case: len(custom_mixer) != 1 and != len(gammas) should raise ValueError
+    bad_mixers = SVP_mixers[:-1]  # Make the list the wrong length
+    with pytest.raises(ValueError, match="Invalid custom_mixer length."):
+        QUBO(0, numeric_qubo).train_QAOA(
+            gammas=gammas,
+            betas=betas,
+            alphas=alphas,
+            nshots=10,
+            regular_loss=reg_loss,
+            cvar_delta=cvar_delta,
+            custom_mixer=bad_mixers,
+        )
+
+    # Normal case
     result = QUBO(0, numeric_qubo).train_QAOA(
         gammas=gammas,
         betas=betas,
